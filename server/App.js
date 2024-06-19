f1e234b75afe6698f77df89c9bb4254093fe9291
TWINKLE PIE <22twinklepie@gmail.com>
2024-06-19 13:24:19 +0530

    bye-bye

--- server/App.js
+++ server/App.js
@@ -23,3 +23,18 @@ app.get('/greet/:name', function (req, res) {
   const name = req.params.name;
   res.send(`Hello, ${name}! Welcome to our site.`);
 });
+app.get('/goodbye', function (req, res) {
+  res.set('Content-Type', 'text/html');
+  res.status(200).send("<h1>Goodbye World</h1>");
+});
+
+app.get('/quote', function (req, res) {
+  const quotes = [
+    "The best way to predict the future is to invent it.",
+    "Life is 10% what happens to us and 90% how we react to it.",
+    "The only way to do great work is to love what you do.",
+    "If you can dream it, you can achieve it."
+  ];
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  res.send(`Quote of the day: ${randomQuote}`);
+});
