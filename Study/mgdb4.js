e913213731f40da2205772e3d8f124ed5c1333b7
TWINKLE PIE <22twinklepie@gmail.com>
2024-07-10 04:55:13 +0530

    .

--- Study/mgdb4.js
+++ Study/mgdb4.js
@@ -7,6 +7,7 @@ app.use(express.json());
 
 // 1. Set up MongoDB connection
 mongoose.connect('mongodb://localhost:27017/mylibrary', { useNewUrlParser: true, useUnifiedTopology: true });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
